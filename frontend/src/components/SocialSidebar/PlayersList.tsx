adb614049ae256651c61c45374f4b76321146b89
LAnselet <yichen.sam.sun@hotmail.com>
2022-03-31 15:55:07 -0400

    Update SocialSidebar components

--- frontend/src/components/SocialSidebar/PlayersList.tsx
+++ frontend/src/components/SocialSidebar/PlayersList.tsx
@@ -1,5 +1,9 @@
+import { Box, Heading, ListItem, OrderedList, Tooltip } from '@chakra-ui/react';
 import React from 'react';
 
+import useCoveyAppState from '../../hooks/useCoveyAppState';
+import usePlayersInTown from '../../hooks/usePlayersInTown';
+import PlayerName from './PlayerName';
 
 /**
  * Lists the current players in the town, along with the current town's name and ID
@@ -11,8 +15,28 @@ import React from 'react';
  * Each player is rendered in a list item, rendered as a <PlayerName> component
  * 
  * See `usePlayersInTown` and `useCoveyAppState` hooks to find the relevant state.
+ * See relevant hooks: `usePlayersInTown` and `useCoveyAppState` 
  * 
  */
 export default function PlayersInTownList(): JSX.Element {
   return <></>
+  const players = usePlayersInTown();
+  const { currentTownFriendlyName, currentTownID } = useCoveyAppState();
+  const sorted = players.concat([]);
+  sorted.sort((p1, p2) => p1.userName.localeCompare(p2.userName, undefined, { numeric: true, sensitivity: 'base' }));
+
+  return (
+    <Box><Tooltip label={`Town ID: ${currentTownID}`}>
+      <Heading as='h2' fontSize='l'>
+        Current town: {currentTownFriendlyName}
+      </Heading></Tooltip>
+      <OrderedList>
+        {sorted.map(player => (
+          <ListItem key={player.id}>
+            <PlayerName player={player} />
+          </ListItem>
+        ))}
+      </OrderedList>
+    </Box>
+  );
 }
